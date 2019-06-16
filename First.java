71cc79aae439f02f4853a26ba18939d1eebdb0d4
noella <noellanzeba@gmail.com>
2019-06-16 16:26:55 +0100

    changes to trigger Jenkins job to run

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
 public class First{
     public static void main(String[] args) {
-        System.out.println("Hey Jose. Welcome to GIT anf Jenkins");
+        System.out.println("This is the first project in Jenkins");
     }
 }
--- FirstGit.java
+++ FirstGit.java
@@ -1,5 +1,7 @@
 public class FirstGit{
     public static void main(String[] args) {
         System.out.println("Running from GitHub");
+        System.out.println("This line will trigger Jenkins to run"); 
     }
+    
 }
